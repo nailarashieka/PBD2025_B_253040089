86e9bc4acd5f7809d218c4e8d3119313aa7af4e5
nailarashieka <nailashieka@gmail.com>
2025-12-05 08:58:20 +0700

    Add files via upload

--- PBD2025_B_253040089/Pertemuan 3/SQLQuery1.sql
+++ PBD2025_B_253040089/Pertemuan 3/SQLQuery1.sql
@@ -25,3 +25,88 @@ WHERE Color = 'Red';
 SELECT Name + ' (' + Color + ')' AS Warna 
 FROM Production.Product 
 WHERE Color = 'Black'; 
+
+SELECT Name, ListPrice 
+FROM Production.Product 
+WHERE ListPrice > 1000; 
+
+SELECT Name, Color, ListPrice 
+FROM Production.Product 
+WHERE Color = 'Black' AND ListPrice > 500; 
+
+SELECT Name, Color 
+FROM Production.Product 
+WHERE Color IN ('Red', 'Blue', 'Black'); 
+
+SELECT Name, ProductNumber 
+FROM Production.Product 
+WHERE Name LIKE '%Road%'; 
+
+SELECT COUNT(*) AS TotalProduk 
+FROM Production.Product;
+
+SELECT Color, COUNT(*) AS JumlahProduk 
+FROM Production.Product 
+GROUP BY Color; 
+
+SELECT ProductID, SUM(OrderQty) AS TotalTerjual, AVG(UnitPrice) AS 
+RataRataHarga 
+FROM Sales.SalesOrderDetail 
+GROUP BY ProductID; 
+
+SELECT Color, Size, COUNT(*) AS Jumlah 
+FROM Production.Product 
+GROUP BY Color, Size; 
+
+SELECT Color, Name, COUNT(*)  
+FROM Production.Product 
+GROUP BY Color; 
+
+SELECT Color, COUNT(*) AS Jumlah 
+FROM Production.Product 
+GROUP BY Color 
+HAVING COUNT(*) > 20; 
+
+SELECT Color, COUNT(*) AS Jumlah 
+FROM Production.Product 
+WHERE ListPrice > 500  -- Filter baris dulu (Step 2) 
+GROUP BY Color         -- Kelompokkan sisa baris (Step 3) 
+HAVING COUNT(*) > 10;  -- Filter hasil kelompok (Step 4) 
+
+SELECT ProductID, SUM(OrderQty) AS TotalQty 
+FROM Sales.SalesOrderDetail 
+GROUP BY ProductID 
+HAVING SUM(OrderQty) > 1000; 
+
+SELECT SpecialOfferID, AVG(OrderQty) AS RataRataBeli 
+FROM Sales.SalesOrderDetail 
+GROUP BY SpecialOfferID 
+HAVING AVG(OrderQty) < 2; 
+
+SELECT Color  
+FROM Production.Product 
+GROUP BY Color 
+HAVING MAX(ListPrice) > 3000;
+
+SELECT DISTINCT JobTitle 
+FROM HumanResources.Employee; 
+
+SELECT Name, ListPrice 
+FROM Production.Product 
+ORDER BY ListPrice DESC; 
+
+SELECT TOP 5 Name, ListPrice 
+FROM Production.Product 
+ORDER BY ListPrice DESC; 
+
+SELECT Name, ListPrice 
+FROM Production.Product 
+ORDER BY ListPrice DESC 
+OFFSET 10 ROWS 
+FETCH NEXT 5 ROWS ONLY;
+
+SELECT TOP 3 Color, SUM(ListPrice) AS TotalNilaiStok 
+FROM Production.Product 
+WHERE ListPrice > 0          
+GROUP BY Color               -- Step 2: Filter sampah -- Step 3: Kelompokkan 
+ORDER BY TotalNilaiStok DESC;-- Step 6: Urutkan 
